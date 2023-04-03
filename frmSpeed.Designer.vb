342dd169ed842f42a89b4221f6c1eb454bb5908d
FLogan1991 <furlong_logan@columbusstate.edu>
2023-04-02 23:22:33 -0400

    Add project files.

---
+++ frmSpeed.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSpeedTest
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSpeedTest))
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.btnSpeed = New System.Windows.Forms.Button()
+        Me.lstSpeed = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblSpeed = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblHeader
+        '
+        Me.lblHeader.AutoSize = True
+        Me.lblHeader.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblHeader.Font = New System.Drawing.Font("Arial", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.Location = New System.Drawing.Point(341, 49)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(320, 29)
+        Me.lblHeader.TabIndex = 1
+        Me.lblHeader.Text = "Internet Speed Test Survey"
+        '
+        'btnSpeed
+        '
+        Me.btnSpeed.BackColor = System.Drawing.Color.DarkKhaki
+        Me.btnSpeed.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSpeed.Location = New System.Drawing.Point(386, 95)
+        Me.btnSpeed.Name = "btnSpeed"
+        Me.btnSpeed.Size = New System.Drawing.Size(206, 41)
+        Me.btnSpeed.TabIndex = 2
+        Me.btnSpeed.Text = "Enter Internet Speed"
+        Me.btnSpeed.UseVisualStyleBackColor = False
+        '
+        'lstSpeed
+        '
+        Me.lstSpeed.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstSpeed.FormattingEnabled = True
+        Me.lstSpeed.ItemHeight = 22
+        Me.lstSpeed.Location = New System.Drawing.Point(725, 95)
+        Me.lstSpeed.Name = "lstSpeed"
+        Me.lstSpeed.Size = New System.Drawing.Size(65, 158)
+        Me.lstSpeed.TabIndex = 3
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(682, 400)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lblSpeed
+        '
+        Me.lblSpeed.AutoSize = True
+        Me.lblSpeed.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblSpeed.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSpeed.Location = New System.Drawing.Point(435, 523)
+        Me.lblSpeed.Name = "lblSpeed"
+        Me.lblSpeed.Size = New System.Drawing.Size(174, 20)
+        Me.lblSpeed.TabIndex = 5
+        Me.lblSpeed.Text = "This is a Placeholder"
+        '
+        'frmSpeedTest
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(834, 561)
+        Me.Controls.Add(Me.lblSpeed)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstSpeed)
+        Me.Controls.Add(Me.btnSpeed)
+        Me.Controls.Add(Me.lblHeader)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "frmSpeedTest"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Internet Speed Test Survey for Home Users"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents btnSpeed As Button
+    Friend WithEvents lstSpeed As ListBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblSpeed As Label
+End Class
---
+++ frmSpeed.vb
@@ -0,0 +1,63 @@
+﻿Public Class frmSpeedTest
+    Dim strSpeed As String
+    Dim decSpeed As Decimal
+    Dim decSumOfSpeeds As Decimal
+    Dim decAverage As Decimal = 0D
+
+    Dim strIBoxMsg As String = "Enter the number of Mbps of your home Internet speed user #"
+    Dim strIBoxTitle As String = "Internet Speed"
+    Dim strNotNumericErrMsg As String = "Error - Enter the speed of your home Internet connection"
+    Dim strNegErrMsg As String = "Error - Enter a valid speed"
+
+    Dim intMaxEntries As Integer = 10
+    Dim intEntries As Integer = 1
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click, MyBase.Load
+
+        'clears the labels, listbox, and enables btnSpeed is also used on load of the form
+        lstSpeed.Items.Clear()
+        lblSpeed.Text = ""
+        btnSpeed.Enabled = True
+
+    End Sub
+
+    Private Sub btnSpeed_Click(sender As Object, e As EventArgs) Handles btnSpeed.Click
+
+        'Takes input from the user and initializes the variable
+        strSpeed = InputBox(strIBoxMsg & intEntries, strIBoxTitle)
+
+        'Do loop set for the number of entries to not pass the number of max entries
+        Do Until intEntries > intMaxEntries Or strSpeed = ""
+            If IsNumeric(strSpeed) Then
+                decSpeed = Convert.ToDecimal(strSpeed)
+                strIBoxMsg = "Enter the number of Mbps of your home Internet speed user #"
+                If decSpeed > 0 Then
+                    lstSpeed.Items.Add(decSpeed)
+                    decSumOfSpeeds += decSpeed
+                    intEntries += 1
+                    strIBoxMsg = "Enter the number of Mbps of your home Internet speed user #"
+                Else
+                    strIBoxMsg = strNegErrMsg
+                End If
+            Else
+                strIBoxMsg = strNotNumericErrMsg
+            End If
+            If intEntries <= intMaxEntries Then
+                strSpeed = InputBox(strIBoxMsg & intEntries, strIBoxTitle)
+            End If
+        Loop
+
+        lblSpeed.Visible = True
+
+        'calculates and displays the average of the speed
+        If intEntries > 1 Then
+            decAverage = decSumOfSpeeds / (intEntries - 1)
+            lblSpeed.Text = "Average Internet Speed: " & decAverage.ToString("F2") & " Mbps "
+        Else
+            lblSpeed.Text = "No Speed Values Entered"
+        End If
+
+
+        btnSpeed.Enabled = False
+    End Sub
+End Class
